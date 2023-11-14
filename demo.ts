1ea3f4621a1d5f1b1d669517ddbe19236ce00096
0xarthurxyz <46296830+0xarthurxyz@users.noreply.github.com>
2023-11-14 11:20:27 +0000

    feat(voter rewards): adds logic to fetch votes cast by an account for a group, and adds filter to fetch epoch distributions to group

--- demo.ts
+++ demo.ts
@@ -6,6 +6,7 @@ import {
     parseAbiItem,
     getAddress,
     formatEther,
+    getContract,
 } from "viem";
 import { celo } from "viem/chains";
 import {
@@ -50,7 +51,7 @@ const publicClient = createPublicClient({
     transport: http(),
 });
 
-async function getEpochDistributions() {
+async function getLogsOfLastEpoch() {
     try {
         const currentBlockNumber = await publicClient.getBlockNumber();
         console.log(`Current block number: ${currentBlockNumber}`);
@@ -130,8 +131,59 @@ async function getEpochDistributions() {
     }
 }
 
+/**
+ * Incomplete (WIP)
+ * */
 async function getVoterRewards(epochNumber: bigint) {
-    // TODO
+    // Test: Get active votes of an account
+    const electionContract = getContract({
+        address: "0x8d6677192144292870907e3fa8a5527fe55a7ff6",
+        abi: electionABI,
+        publicClient,
+    });
+    const RANDOM_VOTER = "0xbd5cac2afcc30d2c32e7a1afdfa85e5f6bb22f98";
+    const RANDOM_VALIDATOR_GROUP = "0x81383e7C8801B102f742f4F5a5faD06867212b05";
+
+    const result = await electionContract.read.getActiveVotesForGroupByAccount([
+        RANDOM_VALIDATOR_GROUP,
+        RANDOM_VOTER,
+    ]);
+    console.log(
+        `${RANDOM_VOTER.substring(0, 8)}... is voting with ${formatEther(
+            result
+        )} CELO for ${RANDOM_VALIDATOR_GROUP.substring(0, 8)}...`
+    );
+
+    console.log(`${RANDOM_VOTER.substring(0, 8)}... is voting with a total of ${formatEther(
+            await electionContract.read.getTotalVotesByAccount([
+                `0xbd5cac2afcc30d2c32e7a1afdfa85e5f6bb22f98`,
+            ])
+        )} CELO`);
+
+    // Test: Get epoch rewards distributed to a group
+    const epochBlock = epochNumber * BLOCKS_PER_EPOCH;
+    const filter = await publicClient.createContractEventFilter({
+        address: "0x8d6677192144292870907e3fa8a5527fe55a7ff6", // Election Proxy contract
+        abi: electionABI,
+        eventName: "EpochRewardsDistributedToVoters",
+        args: {
+            group: RANDOM_VALIDATOR_GROUP,
+        },
+        fromBlock: epochBlock,
+        toBlock: epochBlock,
+    });
+    const logs = await publicClient.getFilterLogs({ filter });
+    console.log(
+        `Summary:`,
+        {
+            epoch: epochNumber,
+            name: "Epoch rewards distribution to group",
+            value: `${formatEther(logs[0].args.value!)} CELO`,
+            to: RANDOM_VALIDATOR_GROUP,
+        },
+        "\n"
+    );
+    console.log(`Detail(s):`, logs, "\n");
 }
 
 async function getCarbonOffsetDistribution(epochNumber: bigint) {
@@ -210,4 +262,4 @@ async function playground() {
     }
 }
 
-getCommunityFundDistribution(1296n);
+getVoterRewards(1296n);
